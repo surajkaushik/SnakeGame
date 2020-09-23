e0916a3a6e4a9946ecdf86d9a943bdd21b7e0d42
Suraj Kaushik <kaushiksuraj1102@gmail.com>
2020-09-24 01:05:59 +0530

    Added a panel of dimension 500*500

--- snakeGame/src/snakeGame/GameMain.java
+++ snakeGame/src/snakeGame/GameMain.java
@@ -6,6 +6,18 @@ public class GameMain {
 	
 	public GameMain() {
 		JFrame frame= new JFrame();
+		
+		GamePanel gamePanel = new GamePanel();
+		
+		frame.add(gamePanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("SnakeGame");
+		
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		
 
 	}
 	
---
+++ snakeGame/src/snakeGame/GamePanel.java
@@ -0,0 +1,35 @@
+package snakeGame;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+
+	public static final int WIDTH=500, HEIGHT=500;
+	
+	public GamePanel() {
+		// TODO Auto-generated constructor stub
+		 setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+
+}
