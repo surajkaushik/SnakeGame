8d23a18c8afa79c0332099c03d53a3bff02ab0d9
Suraj Kaushik <kaushiksuraj1102@gmail.com>
2020-09-24 00:53:31 +0530

    initial Commit

---
+++ snakeGame/src/snakeGame/GameMain.java
@@ -0,0 +1,15 @@
+package snakeGame;
+
+import javax.swing.JFrame;
+
+public class GameMain {
+	
+	public GameMain() {
+		JFrame frame= new JFrame();
+
+	}
+	
+	public static void main(String[] args) {
+	 new GameMain();
+	}
+}
